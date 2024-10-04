a796571899784232c1ab2a51859642c9333eb3cf
Lehman Garrison <lgarrison@flatironinstitute.org>
2024-10-04 12:52:52 -0400

    more portable cache line size detection

--- include/cpp_utils.h
+++ include/cpp_utils.h
@@ -4,6 +4,12 @@
 
 #include "defs.h"
 
+#ifdef __cpp_lib_hardware_interference_size
+    static const size_t CACHELINE = std::hardware_destructive_interference_size;
+#else
+    static const size_t CACHELINE = 64;
+#endif
+
 void raise_python_exception();
 void raise_RuntimeError(const char *msg);
 
